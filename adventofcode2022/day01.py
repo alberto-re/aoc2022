eb174bff623917104679ab10e38ec0f47012a15d
Alberto Re <alberto.re@gmail.com>
2022-12-01 17:10:11 +0100

    Initial import

---
+++ adventofcode2022/day01.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# --- Day 1: Calorie Counting ---
+
+from typing import List
+
+
+def calories_per_elf(lines: List[str]) -> List[int]:
+    aggr = [0]
+    for line in lines:
+        try:
+            aggr[-1] += int(line)
+        except ValueError:
+            aggr.append(0)
+    return aggr
+
+def main():
+    with open("input/day01.txt") as f:
+        lines = [line for line in f]
+
+    print(f"Part one solution: {max(calories_per_elf(lines))}")
+    print(f"Part two solution: {sum(sorted(calories_per_elf(lines), reverse=True)[:3])}")
+
+
+if __name__ == "__main__":
+    main()
